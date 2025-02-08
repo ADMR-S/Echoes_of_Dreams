fd487cbc881dfea46b0b091dfc308383b5539ec4
alai06 <119883173+alai06@users.noreply.github.com>
2025-02-08 11:49:58 +0100

    J'ai crée XRSceneWithHavok4 pour avoir des astéroïdes à la place de boules vertes

--- src/ts/createScene.ts
+++ src/ts/createScene.ts
@@ -5,6 +5,8 @@ import type { Scene } from "@babylonjs/core/scene";
 import { XRSceneWithHavok } from "./scenes/xrSceneWithHavok";
 import { AbstractEngine } from "@babylonjs/core/Engines/abstractEngine";
 import { Player } from "./Player";
+import { XRSceneWithHavok3 } from "./scenes/xrSceneWithHavok3";
+import {XRSceneWithHavok4} from "./scenes/XRSceneWithHavok4";
 
 export interface CreateSceneClass {
     createScene: (engine: AbstractEngine, canvas: HTMLCanvasElement, audiocontext : AudioContext, player : Player) => Promise<Scene>;
