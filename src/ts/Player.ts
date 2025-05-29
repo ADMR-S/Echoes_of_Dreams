670a96022352aa7f759ad5952a1c6d89c4862859
Adam MIR-SADJADI <mirsadjadi2011@yahoo.fr>
2025-05-29 09:17:59 +0200

    Use last position when repositionning

--- src/ts/Player.ts
+++ src/ts/Player.ts
@@ -126,6 +126,7 @@ export class Player{
         objectPickable: Object3DPickable,
         scene: Scene,
         xr: WebXRDefaultExperience,
+        //@ts-ignore
         offsetDistance: number = 0.01
     ) {
         this.resizeAndRepositionObjectObservable = scene.onBeforeRenderObservable.add(() => {
@@ -183,8 +184,8 @@ export class Player{
                     currentOffset *= 1.8;
                     if(i == maxIterations - 1){
                         //Use initial positionning :
-                        this.resizeObject(objectPickable, distance, offsetDistance);
-                        this.displaceObject(objectPickable, ray, offsetDistance, camera, pickResult?.pickedPoint || undefined);
+                        this.resizeObject(objectPickable, distance, Math.abs(ray.direction.scale(-currentOffset).length()));
+                        this.displaceObject(objectPickable, ray, currentOffset, camera, pickResult?.pickedPoint || undefined);
                         console.log("Max iterations reached, using initial positioning");
                     }
                 }
