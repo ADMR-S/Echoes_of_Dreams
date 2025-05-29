ffede87dba1a8c0ad5c0796c42a81810c895e536
Adam MIR-SADJADI <mirsadjadi2011@yahoo.fr>
2025-05-29 05:31:10 +0200

    fix skybox space naming

--- src/ts/scenes/Scene1Superliminal.ts
+++ src/ts/scenes/Scene1Superliminal.ts
@@ -83,7 +83,7 @@ export class Scene1Superliminal implements CreateSceneClass {
 	var skybox = MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
 	var skyboxMaterial = new StandardMaterial("skyBox", scene);
 	skyboxMaterial.backFaceCulling = false;
-	skyboxMaterial.reflectionTexture = new CubeTexture("asset/texture/skybox_space", scene);
+	skyboxMaterial.reflectionTexture = new CubeTexture("asset/texture/skyboxSpace", scene);
 	skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
 	skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
 	skyboxMaterial.specularColor = new Color3(0, 0, 0);
