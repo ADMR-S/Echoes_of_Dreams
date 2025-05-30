66177446b31beebb75cefd4f7dfac7f0a3f85dbd
Adam MIR-SADJADI <mirsadjadi2011@yahoo.fr>
2025-05-30 13:56:03 +0200

    reposition in front of player when no match

--- src/ts/Player.ts
+++ src/ts/Player.ts
@@ -189,22 +189,22 @@ export class Player{
                 if(!this.checkNearbyBoundingBoxes(objectPickable)){
                     // If no nearby bounding boxes, break the loop
                     console.log("CORRECT POSITION FOUND");
-                    console.log("CORRECT Distance to target:", distance, "Offset distance:", offsetLen);
+                    console.log("CORRECT Distance to target:", distance, "Offset distance:", currentOffset);
                     break;
                 } else {
                     //console.log("MESHES INTERSECTING, REPOSITIONNING");
                     currentOffset *= 2;
                     if(i === maxIterations - 1){
                         
-                        
+                        /*
                         //Use initial positionning :
                         this.resizeObject(objectPickable, distance, ray.direction.scale(-offsetDistance/ray.direction.length()).length());
                         this.displaceObject(objectPickable, ray, offsetDistance, camera, pickResult?.pickedPoint || undefined);
                         console.log("Max iterations reached, using initial positioning");
-                        console.log("Distance to target:", distance, "Offset distance:",ray.direction.scale(-offsetDistance/ray.direction.length()).length());
-                        
+                        console.log("Distance to target:", distance, "Offset distance:",offsetDistance);
+                        */
 
-                        /*
+                        
                         // If no valid position found after all attempts, set minimal scale and move close to camera
                         this.resizeObject(objectPickable, distance*0.2, 0);
                         objectPickable.mesh.position = camera.position.add(ray.direction.scale(distance*0.2/ray.direction.length()));
@@ -213,7 +213,6 @@ export class Player{
                         if(this.checkNearbyBoundingBoxes(objectPickable)){
                             console.log("Even minimal scale intersects with object" );
                         }
-                        */
                     }
                 }
             }
